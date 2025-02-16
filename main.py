0556e8358dee2361512f13d27e2e137888abcce7
manuel <manuel@manuel.manuel>
2025-02-16 12:36:14 -0300

    server

---
+++ main.py
@@ -0,0 +1,61 @@
+import yt_dlp
+import sys
+import json
+import os
+import uuid
+
+def get_video_info(video_url):
+    ydl_opts = {
+        'no_warnings': True,
+        'skip_download': True,
+        'noplaylist': True
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(video_url, download=False)
+        video_info = {
+            'url': video_url,
+            'title': info_dict.get('title', None),
+            'thumbnail_url': info_dict.get('thumbnail', None),
+            'author': info_dict.get('uploader', None),
+            'duration': info_dict.get('duration', None),
+        }
+    return video_info
+
+
+def download_audio(video_url, no_play_list, download_path='./'):
+    video_id = str(uuid.uuid4())
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+        'noplaylist': no_play_list,
+        'outtmpl': f'%(title)s_{video_id}.%(ext)s'
+        
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(video_url, download=True)
+        file_path = ydl.prepare_filename(result)
+        file_path = os.path.splitext(file_path)[0] + '.mp3'
+
+    print(file_path)
+    return file_path
+
+if __name__ == "__main__":
+    command = sys.argv[1]
+    no_play_list = sys.argv[2]
+    video_url = sys.argv[3]
+    download_path = sys.argv[4]
+    print("EJECUTANDO")
+
+    if command == 'info':
+        print("ENTRA A INFO")
+        video_info = get_video_info(video_url)
+        print(json.dumps(video_info))
+    elif command == 'download':
+        download_audio(video_url, no_play_list, download_path)
+    else:
+        print('Invalid command')
---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify, send_file, after_this_request
+from flask_cors import CORS
+from main import get_video_info, download_audio
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api', methods=['GET'])
+def info():
+    print(request)
+    video_url = request.args.get('video_url')
+    
+    if not video_url:
+        return jsonify({'error': 'Missing video url parameter'}), 400
+
+    video_info = get_video_info(video_url)
+    return jsonify(video_info)
+
+@app.route('/api', methods=['POST'])
+def download():
+    data = request.get_json()
+    video_url = data.get('video_url')
+    no_play_list = data.get('no_play_list')
+
+    if not video_url:
+        return jsonify({'error': 'Missing video url parameter'}), 400
+
+    file_name = download_audio(video_url, no_play_list=True)
+
+    @after_this_request
+    def remove_file(response):
+        try:
+            os.remove(file_name)
+        except Exception as e:
+            return jsonify({'error': 'Internal server error'}), 500
+        return response
+
+    return send_file(file_name, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
